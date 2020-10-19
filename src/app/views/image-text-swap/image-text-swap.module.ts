44d81c0c6f35b19dcde86d32bd97899549159962
findmunish <findmunish@gmail.com>
2020-10-19 10:16:40 +0530

    3 files modified, 4 added

---
+++ src/app/views/image-text-swap/image-text-swap.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ImageTextSwapComponent } from './image-text-swap/image-text-swap.component';
+
+@NgModule({
+  imports: [CommonModule],
+  exports: [ImageTextSwapComponent],
+  declarations: [ImageTextSwapComponent]
+})
+export class ImageTextSwapModule { }
---
+++ src/app/views/image-text-swap/image-text-swap/image-text-swap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-text-swap',
+  templateUrl: './image-text-swap.component.html',
+  styleUrls: ['./image-text-swap.component.css']
+})
+export class ImageTextSwapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
