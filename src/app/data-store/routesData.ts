1081d31333a952458466248fe263b882850d8edb
findmunish <findmunish@gmail.com>
2020-10-19 10:52:42 +0530

    2 files modified

--- src/app/data-store/routesData.ts
+++ src/app/data-store/routesData.ts
@@ -8,3 +8,24 @@ export const ROUTE_LINKS = {
   ABOUT: '/about-us',
   CONTACT: '/contact-us'
 };
+
+export function GET_ROUTING_LINK(strRouteText) {
+  switch(strRouteText.toUpperCase()) {
+    case 'HOME':
+      return ROUTE_LINKS.HOME;
+    case 'WINDOWS':
+      return ROUTE_LINKS.WINDOWS;
+    case 'DOORS':
+      return ROUTE_LINKS.DOORS;
+    case 'REQ-QUOTE':
+      return ROUTE_LINKS.REQ_QUOTE;
+    case 'REQ_COST_CALC':
+      return ROUTE_LINKS.REQ_COST_CALC;
+    case 'ABOUT':
+      return ROUTE_LINKS.ABOUT;
+    case 'CONTACT':
+      return ROUTE_LINKS.CONTACT;
+    default:
+      return ROUTE_LINKS.HOME;
+  }
+}
