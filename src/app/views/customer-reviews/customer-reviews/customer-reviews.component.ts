bb225c3bb4f134887d876061dd7b2e7ba04306f4
findmunish <findmunish@gmail.com>
2020-10-20 14:54:41 +0530

    modified files

--- src/app/views/customer-reviews/customer-reviews/customer-reviews.component.ts
+++ src/app/views/customer-reviews/customer-reviews/customer-reviews.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-customer-reviews',
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-reviews.component.css']
 })
 export class CustomerReviewsComponent implements OnInit {
-
+  @Input() reviewContent
   constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
